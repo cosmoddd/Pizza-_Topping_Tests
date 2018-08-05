71b9a7749411c76f5c11d9a8bf3934ab5740265b
Unknown <gregheff@gmail.com>
2018-08-05 19:19:53 -0400

    first data

---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+	public int poolingAmount;
+	public GameObject objectPrefab;
+	public float sprinkleRate;
+	// Use this for initialization
+	void Start () {
+		
+		// loop / load prefabs into a large list, all disabled
+		sprinkleRate = 3;
+	}
+	
+
+	void Update() 
+	{
+		if (Input.GetButton("Fire1") && Time.time > sprinkleRate) 
+		{
+		    
+			sprinkleRate = Time.time + sprinkleRate;
+			GameObject clone = Instantiate(objectPrefab, transform.position, transform.rotation) as GameObject;
+		}
+
+	}
+
+	// 
+
+}
